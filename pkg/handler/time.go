fc7aa99c483be8b7879eb7ec009a1a9c6784e1a5
Tarlex <Tarlex.py@yandex.ru>
2020-11-13 16:47:56 +0300

    add getNow and getString

--- pkg/handler/time.go
+++ pkg/handler/time.go
@@ -2,29 +2,53 @@ package handler
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func actualtime(t time.Time) float64 {
-	h := float64(t.Hour())
-	hh := float64(t.Hour())
-	m := float64(t.Minute())
-	s := float64(t.Second())
-	fldata := []float64{h, m, s}
-	fmt.Print(fldata)
-	return hh
+	dt := time.Now()
+	fmt.Println(dt)
+
+	// месяц день год поменять на день месяц год по заданию
+	fdt := dt.Format("010206.150405")
+
+	fmt.Println(fdt)
+	// Перевод строки во float64
+	var x float64 = 0
+	if fdt, err := strconv.ParseFloat(fdt, 64); err == nil {
+		x = fdt
+	}
+	return x
 }
 
 func (h *Handler) getNow(c *gin.Context) {
 	t := time.Now()
-	// time := t.Format("03:04:05")
 	c.JSON(200, gin.H{"time": actualtime(t)})
 }
 
-func (h *Handler) getString(c *gin.Context) {
+func stringTime(t time.Time) string {
 
+	// 20 октября 2018 года,19 часов,35 минут, 21 секунда.
+	dateStr := fmt.Sprintf(
+		"%v %v %v года, %v часов %v минут %v секунда",
+		t.Day(), t.Month(), t.Year(), t.Hour(), t.Minute(), t.Second(),
+	)
+	return string(dateStr)
+}
+
+func (h *Handler) getString(c *gin.Context) {
+	r := c.DefaultQuery("time", "Guest")
+	tr, err := time.Parse("010206.150405", r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	date := stringTime(tr)
+	c.JSON(200, gin.H{"str": date})
 }
 
 func (h *Handler) getAdd(c *gin.Context) {
